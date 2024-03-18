7d77e711ed2041c3a5b272c411149849ae96938c
Christine Tham <chris.tham@hellotham.com>
2024-03-18 20:53:00 +1100

    Fixed Newsflash responsiveness

--- src/components/Home/NewsFlash.jsx
+++ src/components/Home/NewsFlash.jsx
@@ -15,24 +15,26 @@ export default function NewsFlash({ article }) {
     <div id="news-flash">
       <h1>{article.data.title}</h1>
 
-      <div id="news-flash-wrapper" style={{ display: 'flex', flexDirection: 'row' }}>
-        <div style={{ paddingBottom: '16px' }}>
-          <p style={{ fontStyle: 'italic', fontWeight: 'bold', color: 'var(--darkpurple)' }}>
+      <div id="news-flash-wrapper" className="row align-items-lg-stretch">
+      <div className="col-lg-6 pb-2">
+        <p style={{ fontStyle: 'italic', fontWeight: 'bold', color: 'var(--darkpurple)' }}>
             {article.data.subtitle}
           </p>
           <Markdown>{article.body}</Markdown>
           <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <CustomButton text="Learn More" redirect_link={article.data.redirectLink} />
           </div>
-        </div>
+      </div>
+      <div className="col-lg-6">
         <img
           id="news-flash-img"
-          style={{ marginLeft: '20px', objectFit: 'cover' }}
-          width="50%"
+          style={{ objectFit: 'contain' }}
+          width="100%"
           src={article.data.image}
           alt={article.data.title}
         />
       </div>
+    </div>
     </div>
   )
 }
